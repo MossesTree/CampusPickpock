74d0c3e574b3c6e12c0fcf9d38c3609e5f8b2dac
Kim Kyengdong <lighttreekim@gmail.com>
2025-09-28 20:21:11 +0900

    Create Project

---
+++ CampusPickPock/CampusPickPockTests/CampusPickPockTests.swift
@@ -0,0 +1,17 @@
+//
+//  CampusPickPockTests.swift
+//  CampusPickPockTests
+//
+//  Created by Kim Kyengdong on 9/28/25.
+//
+
+import Testing
+@testable import CampusPickPock
+
+struct CampusPickPockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
